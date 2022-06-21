319b8ea5b0937e46e61cd77f342e151f95c5fc9c
yang.geng.chen <younggc@foxmail.com>
2022-06-21 11:00:05 +0800

    boot 2.7.0 import

---
+++ boot270/src/main/java/com/yanggc/Boot270Application.java
@@ -0,0 +1,13 @@
+package com.yanggc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot270Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot270Application.class, args);
+    }
+
+}
---
+++ boot270/src/test/java/com/yanggc/Boot270ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanggc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot270ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
