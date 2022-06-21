ad2fc0307283044fd302549f26d0653fc953a8d2
yang.geng.chen <younggc@foxmail.com>
2022-06-21 15:12:01 +0800

    boot 2.7.0 starter

--- boot270/src/main/java/com/yanggc/Boot270Application.java
+++ boot270/src/main/java/com/yanggc/Boot270Application.java
@@ -3,6 +3,11 @@ package com.yanggc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Description:
+ *
+ * @author: YangGC
+ */
 @SpringBootApplication
 public class Boot270Application {
 
