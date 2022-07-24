e311d774242f691e33240f3f95ec99edb9eb1b23
YangGC <younggengchen@gmail.com>
2022-07-25 03:06:21 +0800

    import

--- boot270/src/main/java/com/yanggc/Boot270Application.java
+++ boot270/src/main/java/com/yanggc/Boot270Application.java
@@ -1,7 +1,12 @@
 package com.yanggc;
 
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.spring.core.RocketMQTemplate;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.Resource;
 
 /**
  * Description:
@@ -9,10 +14,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author: YangGC
  */
 @SpringBootApplication
-public class Boot270Application {
+public class Boot270Application implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(Boot270Application.class, args);
     }
 
+    @Resource
+    private RocketMQTemplate rocketMQTemplate;
+
+    private final static String STRING_TOPIC= "string-topic";
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("..................");
+
+        // 发送字符串
+        SendResult sendResult = rocketMQTemplate.syncSend(STRING_TOPIC, "Hello, World!");
+
+    }
 }
